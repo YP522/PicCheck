1838ed5fe600f99413833b207cc2222bb1f70d71
YP522 <yoannpetit22@free.fr>
2023-02-21 18:30:00 +0100

    ♻️ refactor: Optimisation by reducing dependency (pathlib removal)

--- generate/generateFolders.py
+++ generate/generateFolders.py
@@ -1,17 +1,16 @@
+import os
 from system import utils as u
-from pathlib import Path
 
 def create_folders():
     """
     Create folders for the project
     """
-    Path(u.dt_string+"/data/color/normal").mkdir(parents=True, exist_ok=True)
-    Path(u.dt_string+"/data/color/grayscale").mkdir(parents=True, exist_ok=True)
-    Path(u.dt_string+"/report/assets/themes").mkdir(parents=True, exist_ok=True)
+    os.makedirs(u.dt_string+"/data/color/normal", exist_ok=True)
+    os.makedirs(u.dt_string+"/data/color/grayscale", exist_ok=True)
+    os.makedirs(u.dt_string+"/report/assets/themes", exist_ok=True)
 
-    Path(u.dt_string+"/data/dct/img1/tiles/").mkdir(parents=True, exist_ok=True)  
-    Path(u.dt_string+"/data/dct/img2/tiles/").mkdir(parents=True, exist_ok=True) 
+    os.makedirs(u.dt_string+"/data/dct/img1/tiles/", exist_ok=True)  
+    os.makedirs(u.dt_string+"/data/dct/img2/tiles/", exist_ok=True) 
 
-    Path(u.dt_string+"/data/idct/img1/tiles/").mkdir(parents=True, exist_ok=True)  
-    Path(u.dt_string+"/data/idct/img2/tiles/").mkdir(parents=True, exist_ok=True) 
-
+    os.makedirs(u.dt_string+"/data/idct/img1/tiles/", exist_ok=True)  
+    os.makedirs(u.dt_string+"/data/idct/img2/tiles/", exist_ok=True) 
